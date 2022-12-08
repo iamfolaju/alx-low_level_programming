ed80ec60ec98aa2b2fc1cccb5688e94ae6807643
Adefolaju Adegbuyi <rickfabulous201@yahoo.com>
2022-12-08 08:10:30 -0800

    final task

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,7 +2,7 @@
 #include <unistd.h>
 
 /**
-  * main - prints exactly and that piece of art is useful" 
+  * main - prints exactly and that piece of art is useful"
   * - Dora Korpar, 2015-10-19
   * Return: 0
   */
