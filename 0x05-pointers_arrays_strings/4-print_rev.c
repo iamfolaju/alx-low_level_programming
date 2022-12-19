8ad91d000a5a7d7c210f03732c6d719bd93b3494
Adefolaju Adegbuyi <rickfabulous201@yahoo.com>
2022-12-19 04:39:01 -0800

    Final C pointer Task

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+  * print_rev - Print string backwards
+  * @s: String
+  * Return: void
+  */
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++
+	}
+	i--;
+	while (i >= 0)
+	{
+		_putchar(s[i]);
+		i--;
+	}
+	_putchar('\n');
+}
