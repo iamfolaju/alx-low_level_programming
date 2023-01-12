3e258d251d4a6e326a2a5678f246f900c9847443
Adefolaju Adegbuyi <rickfabulous201@yahoo.com>
2023-01-12 12:55:58 +0100

    Alx C More Malloc Free Task Fixed

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * malloc_checked - allocate memory using malloc
+ * @b: bytes allocate
+ * Return: pointer
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(b);
+	if (ptr == NULL)
+		exit(98);
+	return (ptr);
+}
